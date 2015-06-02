49c7229092376b7a248d03c19128d9815391c069
vbarzana <vbarzana@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2015-06-02 12:06:03 +0000

    Updating the year to 2015 in all scripts header license.

--- rte/jWebSocket-1.0/bin/jWebSocketServerLocal.bat
+++ rte/jWebSocket-1.0/bin/jWebSocketServerLocal.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Starting the jWebSocket Server in the current path, 
 echo temporarily overwriting JWEBSOCKET_HOME and JWEBSOCKET_EE_HOME!
-echo (C) Copyright 2010-2014 Innotrade GmbH (jWebSocket.org), Germany (NRW), Herzogenrath
+echo (C) Copyright 2010-2015 Innotrade GmbH (jWebSocket.org), Germany (NRW), Herzogenrath
 echo.
 
 pushd ..
