75c99198e82ba8d65495957e0d0ef7cd8e04e877
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2015-06-04 13:31:41 +0000

    Fixing license headers.

--- branches/jWebSocket-1.0/shared/J2SE/jWebSocketServer/org/jwebsocket/http/MongoDBConnectorsManager.java
+++ branches/jWebSocket-1.0/shared/J2SE/jWebSocketServer/org/jwebsocket/http/MongoDBConnectorsManager.java
@@ -1,5 +1,5 @@
 // ---------------------------------------------------------------------------
-// jWebSocket - RESTConnectorsManager (Community Edition, CE)
+// jWebSocket - MongoDBConnectorsManager (Community Edition, CE)
 //	---------------------------------------------------------------------------
 //	Copyright 2010-2015 Innotrade GmbH (jWebSocket.org)
 //	Alexander Schulze, Germany (NRW)
