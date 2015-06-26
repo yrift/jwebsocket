5438af33e57fb94bde1e72c4507dfc39a0bf6f7e
vbarzana <vbarzana@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2015-06-26 07:04:38 +0000

    Now the Monitoring PlugIn check is also done in the demo Monitoring with Gauges.

--- branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsMonitoring/js/bindows/wMonitoring.js
+++ branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsMonitoring/js/bindows/wMonitoring.js
@@ -21,9 +21,13 @@
  * @author Orlando Miranda Gómez, vbarzana
  */
 $.widget("jws.monitoring", {
-	_init: function( ) {
+	_init: function ( ) {
 		this.NS = jws.NS_BASE + ".plugins.monitoring";
+		this.NS_SYSTEM = jws.NS_BASE + ".plugins.system";
 		this.NS_STREAMING = jws.NS_BASE + ".plugins.streaming";
+		this.MONITORING_PLUGIN_ID = "MonitoringPlugIn";
+		// Used to get the loaded plugins from the server side
+		this.TT_GET_SERVER_PLUGINS = "getPlugInsInfo";
 		this.TT_REGISTER = "register";
 		this.TT_UNREGISTER = "unregister";
 		this.TT_INFO = "computerInfo";
@@ -34,17 +38,38 @@ $.widget("jws.monitoring", {
 		w.monitoring = this;
 		w.monitoring.doWebSocketConnection( );
 	},
-	doWebSocketConnection: function( ) {
+	doWebSocketConnection: function ( ) {
+		var lMe = this;
 		// Each widget uses the same authentication mechanism, please refer
 		// to the public widget ../../res/js/widgets/wAuth.js
 		var lCallbacks = {
-			OnOpen: function() {
-
-			},
-			OnClose: function() {
+			OnClose: function () {
 				w.monitoring.resetGauges();
 			},
-			OnLogon: function() {
+			OnLogon: function () {
+				var lToken = {
+					ns: lMe.NS_SYSTEM,
+					type: lMe.TT_GET_SERVER_PLUGINS
+				};
+				mWSC.sendToken(lToken, {
+					OnSuccess: function (aToken) {
+						var lMonitoringLoaded = false;
+						for (var lIdx = 0; lIdx < aToken.data.length; lIdx++) {
+							if (lMe.MONITORING_PLUGIN_ID === aToken.data[lIdx].id) {
+								lMonitoringLoaded = true;
+								break;
+							}
+						}
+						if (!lMonitoringLoaded) {
+							jwsDialog("Please make sure that you properly configured " +
+									"the Monitoring PlugIn in the server side " +
+									"configuration file, by following the instructions " +
+									"<a href='http://jwebsocket.org/documentation/Plug-Ins/"+
+									"Monitoring-Plug-In/Administrator-Guide' target='_blank'>from our website</a>.",
+									"Monitoring PlugIn not loaded!", true, "alert");
+						}
+					}
+				});
 				if (mWSC && mWSC.streaming) {
 					mWSC.streaming.registerStream("timeStream");
 				}
@@ -57,7 +82,7 @@ $.widget("jws.monitoring", {
 				// Sending the register token
 				mWSC.sendToken(lRegisterToken);
 			},
-			OnLogoff: function() {
+			OnLogoff: function () {
 				mWSC.streaming.unregisterStream("timeStream");
 				var lUnregisterToken = {
 					ns: w.monitoring.NS,
@@ -68,12 +93,7 @@ $.widget("jws.monitoring", {
 				mWSC.sendToken(lUnregisterToken);
 				w.monitoring.resetGauges();
 			},
-			OnWelcome: function(aToken) {
-				if (aToken.username !== "anonymous") {
-					this.OnLogon(aToken);
-				}
-			},
-			OnMessage: function(aEvent, aToken) {
+			OnMessage: function (aEvent, aToken) {
 				if (w.monitoring.NS === aToken.ns &&
 						w.monitoring.TT_INFO === aToken.type) {
 					w.monitoring.updateGauge(aToken);
@@ -98,7 +118,7 @@ $.widget("jws.monitoring", {
 		$("#demo_box").auth(lCallbacks);
 	},
 	// Dynamically update the gauge at runtime
-	updateGauge: function(aToken) {
+	updateGauge: function (aToken) {
 		//cpu
 		var IValue = parseInt(aToken.consumeCPU);
 		w.monitoring.mCPUGauge.needle.setValue(IValue);
@@ -123,7 +143,7 @@ $.widget("jws.monitoring", {
 		w.monitoring.mHDDGauge.needle.setValue(IUsed);
 		w.monitoring.mHDDGauge.maxValue.setEndValue(parseInt(aToken.totalHddSpace));
 	},
-	updateTime: function(aToken) {
+	updateTime: function (aToken) {
 		if (aToken) {
 			w.monitoring.mClockGauge.needleMinutes.setValue(aToken.minutes);
 			w.monitoring.mClockGauge.needleSeconds.setValue(aToken.seconds);
@@ -131,7 +151,7 @@ $.widget("jws.monitoring", {
 		}
 	},
 	//Reset gauges when the server is disconnect
-	resetGauges: function() {
+	resetGauges: function () {
 		w.monitoring.mCPUGauge.needle.setValue("0");
 		w.monitoring.mCPUGauge.label.setText("0");
 		w.monitoring.mMemGauge.needle1.setValue(0);
