0be77273ad5ce5c89df2078e9bcabd0d7670ed82
vbarzana <vbarzana@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2015-06-30 13:58:33 +0000

    WIP providing a new method to validate google ReCAPTCHA user selection, so this can be later integrated into the SMS PlugIn. In general ReCAPTCHA lets you embed a CAPTCHA in your web pages in order to protect them against spam and other types of automated abuse.

--- jWebSocketPlugIns/jWebSocketJCaptchaPlugIn/src/main/java/org/jwebsocket/plugins/jcaptcha/JCaptchaPlugIn.java
+++ jWebSocketPlugIns/jWebSocketJCaptchaPlugIn/src/main/java/org/jwebsocket/plugins/jcaptcha/JCaptchaPlugIn.java
@@ -20,14 +20,26 @@ package org.jwebsocket.plugins.jcaptcha;
 
 /**
  *
- * @author mayra, Victor Antonio Barzana Crespo, Alexander Schulze, Rolando Santamaria Maso
+ * @author mayra, Victor Antonio Barzana Crespo, Alexander Schulze, Rolando
+ * Santamaria Maso
  */
 import com.octo.captcha.service.CaptchaServiceException;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.URL;
 import javax.imageio.ImageIO;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.net.ssl.HttpsURLConnection;
 import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.jwebsocket.api.PluginConfiguration;
 import org.jwebsocket.api.WebSocketConnector;
 import org.jwebsocket.config.JWebSocketCommonConstants;
@@ -53,6 +65,13 @@ public class JCaptchaPlugIn extends TokenPlugIn {
 	private final static String COPYRIGHT = JWebSocketCommonConstants.COPYRIGHT_CE;
 	private final static String LICENSE = JWebSocketCommonConstants.LICENSE_CE;
 	private final static String DESCRIPTION = "jWebSocket JCaptchaPlugIn - Community Edition";
+	private final static String TT_GET_CAPTCHA = "getcaptcha";
+	private final static String TT_VALIDATE_CAPTCHA = "validate";
+	private final static String TT_VALIDATE_RE_CAPTCHA = "validaterecaptcha";
+
+	// reCaptcha secret key used to validate the user selection
+	private String mReCaptchaSecretKey = "";
+	private String mReCaptchaRemoteIp = "";
 	private String mImgType = null;
 
 	/**
@@ -65,6 +84,8 @@ public class JCaptchaPlugIn extends TokenPlugIn {
 			mLog.debug("Instantiating JCaptcha plug-in...");
 		}
 		setNamespace(NS_JCAPTCHA);
+		mReCaptchaSecretKey = aConfiguration.getString("recaptcha_secret_key");
+		mReCaptchaRemoteIp = aConfiguration.getString("recaptcha_remote_ip");
 
 		if (mLog.isInfoEnabled()) {
 			mLog.info("JCaptcha plug-in successfully instantiated.");
@@ -110,12 +131,12 @@ public class JCaptchaPlugIn extends TokenPlugIn {
 	public void processToken(PlugInResponse aResponse, WebSocketConnector aConnector, Token aToken) {
 
 		if (aToken.getNS().equals(getNamespace())) {
-			if ("getcaptcha".equals(aToken.getType())) {
+			if (TT_GET_CAPTCHA.equals(aToken.getType())) {
 				mImgType = aToken.getString("imageType");
 				mImgType
 						= mImgType == null
-						? "png"
-						: mImgType.trim().toLowerCase();
+								? "png"
+								: mImgType.trim().toLowerCase();
 
 				if (!mImgType.equalsIgnoreCase("png")
 						&& !mImgType.equalsIgnoreCase("jpg")
@@ -134,12 +155,17 @@ public class JCaptchaPlugIn extends TokenPlugIn {
 		lResponse.setInteger("code", -1);
 
 		if (NS_JCAPTCHA.equals(aToken.getNS())) {
-			if ("validate".equals(aToken.getType())) {
+			if (TT_VALIDATE_CAPTCHA.equals(aToken.getType())) {
 				if (validateCaptcha(aConnector.getSession().getSessionId(), aToken.getString("inputChars"))) {
 					lResponse.setInteger("code", 0);
 				} else {
 					lResponse.setString("msg", "Invalid captcha!");
 				}
+			} else if (TT_VALIDATE_RE_CAPTCHA.equals(aToken.getType())) {
+				// Execute the validation mechanism for re-captcha
+				lResponse.setCode(0);
+				boolean lCaptchaResult = validateReCaptcha(mReCaptchaSecretKey, aToken.getString("g-recaptcha-response"), mReCaptchaRemoteIp);
+				lResponse.setBoolean("success", lCaptchaResult);
 			} else {
 				lResponse.setString("msg", "Operation not supported!");
 			}
@@ -189,5 +215,58 @@ public class JCaptchaPlugIn extends TokenPlugIn {
 		}
 
 		return lValidated;
+	}
+
+	/**
+	 * ReCAPTCHA lets you embed a CAPTCHA in your web pages in order to protect 
+	 * them against spam and other types of automated abuse. This method verifies 
+	 * that a certain user response was true for a known and configured reCaptcha,
+	 * for more information, please visit: https://developers.google.com/recaptcha/docs/verify
+	 * @param aSecret (Required) The configured secret key in the jWebSocketJCaptchaPlugIn settings
+	 * @param aResponse (Required) The response received from the client side returned by the recaptcha js library.
+	 * @param aRemoteIp (Required) The remote ip required by gogle to know from which domain(s) come the captcha validation messages
+	 * @return boolean lResponse, validates whether the captcha is valid or not
+	 */
+	private boolean validateReCaptcha(String aSecret, String aResponse, String aRemoteIp) {
+		boolean lResponse = false;
+		try {
+			URL lURLObj = new URL("https://www.google.com/recaptcha/api/siteverify");
+			HttpsURLConnection lConnection = (HttpsURLConnection) lURLObj.openConnection();
+
+			// add reuqest header
+			lConnection.setRequestMethod("POST");
+			lConnection.setRequestProperty("User-Agent", "Mozilla/5.0");
+			lConnection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+			String lParams = "secret=" + aSecret + "&response="
+					+ aResponse + "&remoteip=" + aRemoteIp;
+
+			// Send post request
+			lConnection.setDoOutput(true);
+			DataOutputStream lOutputStream = new DataOutputStream(lConnection.getOutputStream());
+			lOutputStream.writeBytes(lParams);
+			lOutputStream.flush();
+			lOutputStream.close();
+
+			BufferedReader lInputStreamReader = new BufferedReader(new InputStreamReader(
+					lConnection.getInputStream()));
+			String lInputLine;
+			StringBuilder lJSONResponse = new StringBuilder();
+
+			while ((lInputLine = lInputStreamReader.readLine()) != null) {
+				lJSONResponse.append(lInputLine);
+			}
+			lInputStreamReader.close();
+			//parse JSON response and return 'success' value
+			JSONObject lJSONObject = new JSONObject(lJSONResponse.toString());
+			lResponse = lJSONObject.getBoolean("success");
+		} catch (IOException lEx) {
+			mLog.error(lEx);
+			lResponse = false;
+		} catch (JSONException lEx) {
+			mLog.error(lEx);
+			lResponse = false;
+		}
+		return lResponse;
 	}
 }
