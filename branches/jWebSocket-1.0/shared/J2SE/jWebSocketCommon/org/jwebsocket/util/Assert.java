663dfe278385ea448a5b5a1d9e134230eb633c21
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2015-07-02 12:07:05 +0000

    Introducing a new Assert utility class to remove spring-core dependency from common package.

---
+++ branches/jWebSocket-1.0/shared/J2SE/jWebSocketCommon/org/jwebsocket/util/Assert.java
@@ -0,0 +1,38 @@
+//	---------------------------------------------------------------------------
+//	jWebSocket Assert (Community Edition, CE)
+//	---------------------------------------------------------------------------
+//	Copyright 2010-2015 Innotrade GmbH (jWebSocket.org)
+//	Alexander Schulze, Germany (NRW)
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+//	---------------------------------------------------------------------------
+package org.jwebsocket.util;
+
+/**
+ * Utility class to assert boolean expressions. Useful for method parameters
+ * validation.
+ *
+ * @author Rolando Santamaria Maso
+ */
+public class Assert {
+
+	public static void isTrue(boolean aExpression, String aMessage) {
+		if (!aExpression) {
+			throw new RuntimeException(aMessage);
+		}
+	}
+
+	public static void notNull(Object aObject, String aMessage) {
+		isTrue(null != aObject, aMessage);
+	}
+}
