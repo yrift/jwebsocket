7d78ce21de9851eb0f24dfeaa617f479cbd12ab6
vbarzana <vbarzana@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2015-07-13 08:32:05 +0000

    WIP Updating deployment build version to build 50713 for the release!

--- branches/jWebSocket-1.0/jWebSocketClient/web/res/js/jWebSocket.js
+++ branches/jWebSocket-1.0/jWebSocketClient/web/res/js/jWebSocket.js
@@ -37,9 +37,9 @@ if( window.MozWebSocket ) {
 //:d:en:including various utility methods.
 var jws = {
 
-	//:const:*:VERSION:String:1.0.0 (build 50710)
+	//:const:*:VERSION:String:1.0.0 (build 50713)
 	//:d:en:Version of the jWebSocket JavaScript Client
-	VERSION: "1.0.0 (build 50710)",
+	VERSION: "1.0.0 (build 50713)",
 
 	//:const:*:NS_BASE:String:org.jwebsocket
 	//:d:en:Base namespace
