5024b2c2fa2ec38a2f0f81fe57854780e28fd1aa
vbarzana <vbarzana@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2015-07-29 17:44:58 +0000

    This is an automatic commit generated by our deployment script 0_updateSVNReplaceBuildNumbers.bat in order to put the latest build number 50729 into our repository automatically.

--- jWebSocketClient/web/res/js/jWebSocket_Bundle.js
+++ jWebSocketClient/web/res/js/jWebSocket_Bundle.js
@@ -37,9 +37,9 @@ if( window.MozWebSocket ) {
 //:d:en:including various utility methods.
 var jws = {
 
-	//:const:*:VERSION:String:1.0.0 (build 50717)
+	//:const:*:VERSION:String:1.0.0 (build 50729)
 	//:d:en:Version of the jWebSocket JavaScript Client
-	VERSION: "1.0.0 (build 50717)",
+	VERSION: "1.0.0 (build 50729)",
 
 	//:const:*:NS_BASE:String:org.jwebsocket
 	//:d:en:Base namespace
